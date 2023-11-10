4c2aefd0345fecde0dc4957f6b7e660a474a4308
degohenrique <diegohenriquedevbeckend@gmail.com>
2023-11-10 05:06:32 -0300

    criando banco de dados

--- src/validacao/schema.sql
+++ src/validacao/schema.sql
@@ -19,3 +19,24 @@ VALUES
 ('Moda'),
 ('Bebê'),
 ('Games');
+
+create table produtos (
+  id serial primary key,
+  descricao text not null,
+  quantidade_estoque int,
+  valor decimal(10,2) not null,
+  categoria_id int not null
+);
+
+create table clientes(
+  id serial primary key,
+  nome text not null,
+  email text unique not null,
+  cpf varchar(11) unique not null,
+  cep varchar(8) not null,
+  rua text not null,
+  numero text not null,
+  bairro text not null,
+  cidade text not null,
+  estado text not null
+);
