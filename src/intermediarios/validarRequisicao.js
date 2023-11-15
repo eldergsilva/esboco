5554a3843f91f2e13c9926d396ed346d34c3d00e
degohenrique <diegohenriquedevbeckend@gmail.com>
2023-11-15 16:00:12 -0300

    arrumando error

--- src/intermediarios/validarRequisicao.js
+++ src/intermediarios/validarRequisicao.js
@@ -1,17 +1,9 @@
 
-<<<<<<< HEAD
-const validarRequisicao = (schema) => async (req, res, next) => {    
-     
-=======
 const validarRequisicao = (schema) => async (req, res, next) => {
-
-    console.log('ok')
->>>>>>> 03fc4676be9126f83504b1c09eac014071170758
     try {
         await schema.validateAsync(req.body)
         next();
     } catch (error) {
-        console.log(error)
         return res.status(400).json({
             mensagem: error.message
         })
