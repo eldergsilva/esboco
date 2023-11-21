af1cb100d381d6ae9b80cec8ac6d17438d10d359
degohenrique <diegohenriquedevbeckend@gmail.com>
2023-11-21 20:29:55 -0300

    arrumando error

--- src/controladores/pedidos.js
+++ src/controladores/pedidos.js
@@ -1,41 +1,42 @@
+
 const cadastrarPedido = async (req, res) => {
   try {
     const { cliente_id, observacao, pedido_produtos } = req.body;
 
-     const clienteExistente = await knex('clientes').where('id', cliente_id).first();
+    const clienteExistente = await knex('clientes').where('id', cliente_id).first();
     if (!clienteExistente) {
       return res.status(404).json({ mensagem: 'Cliente não encontrado' });
     }
 
-     const [pedido_id] = await knex('pedidos').insert({
+    const [pedido_id] = await knex('pedidos').insert({
       cliente_id,
       observacao,
-      valor_total: 0,  
+      valor_total: 0,
     }).returning('id');
 
-     for (const produto of pedido_produtos) {
+    for (const produto of pedido_produtos) {
       const produtoExistente = await knex('produtos').where('id', produto.produto_id).first();
       if (!produtoExistente) {
         return res.status(404).json({ mensagem: `Produto com ID ${produto.produto_id} não encontrado` });
       }
 
-       if (produto.quantidade_produto > produtoExistente.quantidade_estoque) {
+      if (produto.quantidade_produto > produtoExistente.quantidade_estoque) {
         return res.status(400).json({ mensagem: `Quantidade insuficiente em estoque para o produto ${produto.produto_id}` });
       }
     }
 
-     await Promise.all(pedido_produtos.map(async (produto) => {
+    await Promise.all(pedido_produtos.map(async (produto) => {
       const { produto_id, quantidade_produto } = produto;
 
-       await knex('produtos')
+      await knex('produtos')
         .where('id', produto_id)
         .decrement('quantidade_estoque', quantidade_produto);
 
-       await knex('pedido_produtos').insert({
+      await knex('pedido_produtos').insert({
         pedido_id: pedido_id[0],
         produto_id,
         quantidade_produto,
-        valor_produto: 0,  
+        valor_produto: 0,
       });
     }));
 
@@ -47,8 +48,8 @@ const cadastrarPedido = async (req, res) => {
 };
 
 
- 
-const listarPedidos =async (req, res) => {
+
+const listarPedidos = async (req, res) => {
   try {
     const pedidos = await knex('pedidos').select('*');
     return res.status(200).json(pedidos);
@@ -58,6 +59,6 @@ const listarPedidos =async (req, res) => {
 }
 module.exports = {
   cadastrarPedido,
-  
+
   listarPedidos
 }
